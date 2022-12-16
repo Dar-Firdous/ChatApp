8be8328c9eb791553da3bfd0e6b5ea74fa2df661
Firdous-mindrops <106724511+Firdous-Ur-Rasool@users.noreply.github.com>
2022-12-16 12:04:56 +0530

    Wrap Up Getting started

---
+++ MessangerApp/Controllers/VideoPlayerViewController.swift
@@ -0,0 +1,19 @@
+//
+//  VideoPlayerViewController.swift
+//  MessangerApp
+//
+//  Created by FIRDOUS UR RASOOL on 16/12/22.
+//
+
+import UIKit
+
+class VideoPlayerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
